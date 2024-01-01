f21a8c6e8c448297da4f7d5237e28e85143802e2
Illya Yalovyy <yalovoy@gmail.com>
2023-12-31 17:07:20 -0800

    Add peek_front to Deque

--- src/fourth.rs
+++ src/fourth.rs
@@ -1,4 +1,4 @@
-use std::{rc::Rc, cell::RefCell};
+use std::{rc::Rc, cell::{RefCell, Ref}};
 
 #[derive(Default)]
 pub struct List<T> {
@@ -59,6 +59,11 @@ impl<T> List<T> {
         })
     }
 
+    pub fn peek_front(&self) -> Option<Ref<T>> {
+        self.head.as_ref().map(|node| {
+            Ref::map(node.borrow(), |node| &node.elem)
+        })
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -84,6 +89,15 @@ mod test {
 
         assert_eq!(list.pop_front(), Some(1));
         assert_eq!(list.pop_front(), None);       
+    }
+
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert!(list.peek_front().is_none());
+        list.push_front(1); list.push_front(2); list.push_front(3);
+
+        assert_eq!(*list.peek_front().unwrap(), 3);
     }
 }
 
