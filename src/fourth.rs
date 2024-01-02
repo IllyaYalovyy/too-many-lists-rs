2cd372db0e6f5bcc48f779ecca2c7a5dfef3be05
Illya Yalovyy <yalovoy@gmail.com>
2024-01-01 17:35:11 -0800

    Remove unnecessary comments from tests

--- src/fourth.rs
+++ src/fourth.rs
@@ -157,53 +157,40 @@ mod test {
     fn basics() {
         let mut list = List::new();
 
-        // Check empty list behaves right
         assert_eq!(list.pop_front(), None);
 
-        // Populate list
         list.push_front(1);
         list.push_front(2);
         list.push_front(3);
 
-        // Check normal removal
         assert_eq!(list.pop_front(), Some(3));
         assert_eq!(list.pop_front(), Some(2));
 
-        // Push some more just to make sure nothing's corrupted
         list.push_front(4);
         list.push_front(5);
 
-        // Check normal removal
         assert_eq!(list.pop_front(), Some(5));
         assert_eq!(list.pop_front(), Some(4));
 
-        // Check exhaustion
         assert_eq!(list.pop_front(), Some(1));
         assert_eq!(list.pop_front(), None);
 
-        // ---- back -----
 
-        // Check empty list behaves right
         assert_eq!(list.pop_back(), None);
 
-        // Populate list
         list.push_back(1);
         list.push_back(2);
         list.push_back(3);
 
-        // Check normal removal
         assert_eq!(list.pop_back(), Some(3));
         assert_eq!(list.pop_back(), Some(2));
 
-        // Push some more just to make sure nothing's corrupted
         list.push_back(4);
         list.push_back(5);
 
-        // Check normal removal
         assert_eq!(list.pop_back(), Some(5));
         assert_eq!(list.pop_back(), Some(4));
 
-        // Check exhaustion
         assert_eq!(list.pop_back(), Some(1));
         assert_eq!(list.pop_back(), None);
     }
@@ -230,11 +217,9 @@ mod test {
     fn test_push_back() {
         let mut list = List::new();
 
-        // Validate adding to empty list
         list.push_back(1);
         assert_eq!(&*list.peek_back().unwrap(), &1);
 
-        // Validate adding to non-empty list
         list.push_back(2);
         assert_eq!(&*list.peek_back().unwrap(), &2);
     }
@@ -243,15 +228,12 @@ mod test {
     fn test_pop_back() {
         let mut list = List::new();
 
-        // Validate popping from empty list
         assert_eq!(list.pop_back(), None);
 
-        // Validate popping single element
         list.push_back(1);
         assert_eq!(list.pop_back(), Some(1));
         assert_eq!(list.pop_back(), None);
 
-        // Validate popping last element
         list.push_back(1);
         list.push_back(2);
         assert_eq!(list.pop_back(), Some(2));
